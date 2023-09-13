a580ae4f76a9809e6b447490d91ef0ec01ece943
Samuele Amisano <65444098+Samu-Amy@users.noreply.github.com>
2023-09-13 08:54:12 +0200

    Removed REI Compatibility (no more issues) fr

--- src/main/java/net/samu/mineloween/screen/GemstoneGrinderScreen.java
+++ src/main/java/net/samu/mineloween/screen/GemstoneGrinderScreen.java
@@ -27,6 +27,7 @@ public class GemstoneGrinderScreen extends HandledScreen<GemstoneGrinderScreenHa
         RenderSystem.setShader(GameRenderer::getPositionTexProgram);
         RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
         RenderSystem.setShaderTexture(0, TEXTURE);
+
         int x = (width - backgroundWidth) / 2;
         int y = (height - backgroundHeight) / 2;
 
